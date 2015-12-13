a51209403ef71746ed51ab8c1d2f609826a8e30b
Jason Brillon <Jason62513@gmail.com>
2015-12-13 16:37:17 -0500

    site minus photos of game

--- twlSite/shadows.js
+++ twlSite/shadows.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
     $('#boxShadow').css('pointer-events', 'all');
     $('#boxShadow').animate({
       opacity: 1
-    }, 5000);
-  }, 3000);
+    }, 3000);
+  }, 2000);
    
   var $shadow = $('#shadow');
   var $boxShadow = $('#boxShadow');
@@ -45,8 +45,26 @@ $(document).ready(function(){
 
   $('#boxShadow').bind('click', function(){ 
     $('body').animate({
-      backgroundColor: '#000000'
-    }, 3000);
+      backgroundColor: '#000000',
+    }, 2250);
+    $('h1').animate({
+      color: '#cc0000'
+    }, 2250);
+    $('#title').animate({
+      top: 0,
+      marginTop: -50
+    }, 2250);
+    $(this).animate({
+      opacity: 0
+    }, 2250)
+    window.setTimeout(function() {
+      $('#title').css('position', 'relative');
+      $('#boxShadow').css('display', 'none');
+      $('#title').css('margin-left', '6px'); // weird offset glitch.
+      $('#content').css('display', 'block');
+      $('body').css('cursor', 'auto');
+      document.getElementById('shadow').removeAttr('shadow');
+    }, 2250);
   });
 
 }); 
